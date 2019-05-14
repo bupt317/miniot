389a3c361f52c5164b3df2fbe8a0d6da80976057
Feinimosi <cyhkkha@gmail.com>
2019-05-14 10:54:46 +0800

    init

---
+++ src/main/java/com/bupt317/miniot/App.java
@@ -0,0 +1,14 @@
+package com.bupt317.miniot;
+
+/**
+ * Create by 菲尼莫斯 on 2019/5/13
+ * Email: cyhkkha@gmail.com
+ * File name: App
+ * Program : miniot
+ * Description :
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Minimized Internet of Things");
+    }
+}
